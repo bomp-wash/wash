8117818ca4b059ce007fb9ac972cc6deb6837c19
Bean <127276945+TIHBean@users.noreply.github.com>
2024-06-17 12:04:43 +0800

    6/17

---
+++ laundry/laundry/line login/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LINE Login</title>
+</head>
+<body>
+    <a href="line_login.php"><img src="https://developers.line.biz/assets/img/web/channel-login-button/line-01.png"></a>
+</body>
+</html>
